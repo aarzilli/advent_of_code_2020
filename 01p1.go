92c6831f7a6ccf656d0dd16f331fef1fda0257fb
aarzilli <alessandro.arzilli@gmail.com>
2020-12-01 07:37:26 +0100

    day 1

---
+++ 01p1.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"os"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// returns x without the last character
+func nolast(x string) string {
+	return x[:len(x)-1]
+}
+
+// splits a string, trims spaces on every element
+func splitandclean(in, sep string, n int) []string {
+	v := strings.SplitN(in, sep, n)
+	for i := range v {
+		v[i] = strings.TrimSpace(v[i])
+	}
+	return v
+}
+
+// convert string to integer
+func atoi(in string) int {
+	n, err := strconv.Atoi(in)
+	must(err)
+	return n
+}
+
+// convert vector of strings to integer
+func vatoi(in []string) []int {
+	r := make([]int, len(in))
+	for i := range in {
+		var err error
+		r[i], err = strconv.Atoi(in[i])
+		must(err)
+	}
+	return r
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func exit(n int) {
+	os.Exit(n)
+}
+
+func main() {
+	fmt.Printf("hello\n")
+	buf, err := ioutil.ReadFile("01.txt")
+	must(err)
+	v := []int{}
+	for _, line := range strings.Split(string(buf), "\n") {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		v = append(v, atoi(line))
+	}
+	part1:
+	for i := range v {
+		for j := i+1; j < len(v); j++ {
+			if v[i] + v[j] == 2020 {
+				fmt.Printf("PART 1: %d\n", v[i] * v[j])
+				break part1
+			}
+		}
+	}
+}
