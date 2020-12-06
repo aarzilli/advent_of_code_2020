7c94688f47b529b2cce6e3af7f3043f76d4dac75
aarzilli <alessandro.arzilli@gmail.com>
2020-12-06 11:03:47 +0100

    cleanup

--- 06.go
+++ 06.go
@@ -3,20 +3,13 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strconv"
 	"strings"
-	"os"
 )
 
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-// returns x without the last character
-func nolast(x string) string {
-	return x[:len(x)-1]
 }
 
 // splits a string, trims spaces on every element
@@ -26,35 +19,6 @@ func splitandclean(in, sep string, n int) []string {
 		v[i] = strings.TrimSpace(v[i])
 	}
 	return v
-}
-
-// convert string to integer
-func atoi(in string) int {
-	n, err := strconv.Atoi(in)
-	must(err)
-	return n
-}
-
-// convert vector of strings to integer
-func vatoi(in []string) []int {
-	r := make([]int, len(in))
-	for i := range in {
-		var err error
-		r[i], err = strconv.Atoi(in[i])
-		must(err)
-	}
-	return r
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func exit(n int) {
-	os.Exit(n)
 }
 
 func pf(fmtstr string, args ...interface{}) {
@@ -79,16 +43,15 @@ func main() {
 				m[byte(ch)]++
 			}
 		}
-		
+
 		for _, v := range m {
 			if v == len(people) {
 				tot2++
 			}
 		}
-		//pf("%d\n", len(m))
 		tot += len(m)
 	}
-	
+
 	pf("PART 1: %d\n", tot)
 	pf("PART 2: %d\n", tot2)
 }
