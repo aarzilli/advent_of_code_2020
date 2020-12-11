88b03aa16781c5e4bf4997eb8f89a5d3c0a4df1e
aarzilli <alessandro.arzilli@gmail.com>
2020-12-11 07:02:55 +0100

    cleanup

--- 11.go
+++ 11.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
-	"strconv"
 	"strings"
 )
 
@@ -14,49 +12,6 @@ func must(err error) {
 	}
 }
 
-// returns x without the last character
-func nolast(x string) string {
-	return x[:len(x)-1]
-}
-
-// splits a string, trims spaces on every element
-func splitandclean(in, sep string, n int) []string {
-	v := strings.SplitN(in, sep, n)
-	for i := range v {
-		v[i] = strings.TrimSpace(v[i])
-	}
-	return v
-}
-
-// convert string to integer
-func atoi(in string) int {
-	n, err := strconv.Atoi(in)
-	must(err)
-	return n
-}
-
-// convert vector of strings to integer
-func vatoi(in []string) []int {
-	r := make([]int, len(in))
-	for i := range in {
-		var err error
-		r[i], err = strconv.Atoi(in[i])
-		must(err)
-	}
-	return r
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func exit(n int) {
-	os.Exit(n)
-}
-
 func pf(fmtstr string, args ...interface{}) {
 	fmt.Printf(fmtstr, args...)
 }
@@ -64,41 +19,33 @@ func pf(fmtstr string, args ...interface{}) {
 var M [][]byte
 var M1 [][]byte
 
-func getseat(i, j int) byte {
+func isocc(i, j int) int {
 	if i < 0 || i >= len(M) {
-		return '.'
+		return 0
 	}
 	if j < 0 || j >= len(M[i]) {
-		return '.'
+		return 0
 	}
-	return M[i][j]
-}
-
-func getocc(i, j int) int {
-	if getseat(i, j) == '#' {
+	if M[i][j] == '#' {
 		return 1
 	}
 	return 0
 }
 
 func occupied(i, j int) int {
-	return getocc(i-1, j-1) + getocc(i-1, j) + getocc(i-1, j+1) + getocc(i, j-1) + getocc(i, j+1) + getocc(i+1, j-1) + getocc(i+1, j) + getocc(i+1, j+1)
+	return isocc(i-1, j-1) + isocc(i-1, j) + isocc(i-1, j+1) + isocc(i, j-1) + isocc(i, j+1) + isocc(i+1, j-1) + isocc(i+1, j) + isocc(i+1, j+1)
 }
 
-func step1(i, j int) {
-	if M[i][j] == 'L' && occupied(i, j) == 0 {
-		M1[i][j] = '#'
-	} else if M[i][j] == '#' && occupied(i, j) >= 4 {
-		M1[i][j] = 'L'
-	} else {
-		M1[i][j] = M[i][j]
-	}
-}
-
-func step() {
+func stepP1() {
 	for i := range M {
 		for j := range M[i] {
-			step1(i, j)
+			if M[i][j] == 'L' && occupied(i, j) == 0 {
+				M1[i][j] = '#'
+			} else if M[i][j] == '#' && occupied(i, j) >= 4 {
+				M1[i][j] = 'L'
+			} else {
+				M1[i][j] = M[i][j]
+			}
 		}
 	}
 	M, M1 = M1, M
@@ -130,47 +77,64 @@ func allocc() int {
 func stepP2() {
 	for i := range M {
 		for j := range M[i] {
-			step1P2(i, j)
+			if M[i][j] == 'L' && occupiedP2(i, j) == 0 {
+				M1[i][j] = '#'
+			} else if M[i][j] == '#' && occupiedP2(i, j) >= 5 {
+				M1[i][j] = 'L'
+			} else {
+				M1[i][j] = M[i][j]
+			}
 		}
 	}
 	M, M1 = M1, M
 }
 
-func step1P2(i, j int) {
-	/*if M[i][j] == 'L' {
-		pf("occupied %d\n", occupiedP2(i, j))
-	}*/
-	if M[i][j] == 'L' && occupiedP2(i, j) == 0 {
-		M1[i][j] = '#'
-	} else if M[i][j] == '#' && occupiedP2(i, j) >= 5 {
-		M1[i][j] = 'L'
-	} else {
-		M1[i][j] = M[i][j]
-	}
-}
-
 func occupiedP2(i, j int) int {
-	return getoccP2(i, j, -1, -1) + getoccP2(i, j, -1, 0) + getoccP2(i, j, -1, +1) + getoccP2(i, j, 0, -1) + getoccP2(i, j, 0, +1) + getoccP2(i, j, +1, -1) + getoccP2(i, j, +1, 0) + getoccP2(i, j, +1, +1)
+	return isoccP2(i, j, -1, -1) + isoccP2(i, j, -1, 0) + isoccP2(i, j, -1, +1) + isoccP2(i, j, 0, -1) + isoccP2(i, j, 0, +1) + isoccP2(i, j, +1, -1) + isoccP2(i, j, +1, 0) + isoccP2(i, j, +1, +1)
 }
 
-func getoccP2(si, sj, di, dj int) int {
+func isoccP2(si, sj, di, dj int) int {
 	i := si + di
 	j := sj + dj
 	for i >= 0 && i < len(M) && j >= 0 && j < len(M[i]) {
-		if M[i][j] == '#' {
+		switch M[i][j] {
+		case '#':
 			return 1
-		}
-		if M[i][j] == 'L' {
+		case 'L':
 			return 0
+		default:
+			i += di
+			j += dj
 		}
-		i += di
-		j += dj
 	}
 	return 0
 }
 
+func printmatrix() {
+	for i := range M {
+		pf("%s\n", string(M[i]))
+	}
+	pf("\n")
+}
+
 const debug = false
-const part2 = true
+
+func loop(step func()) {
+	for n := 0; n < 100000; n++ {
+		//pf("%d %d\n", n, allocc())
+		step()
+		if debug {
+			printmatrix()
+		}
+		if nochange() {
+			break
+		}
+	}
+
+	if debug {
+		printmatrix()
+	}
+}
 
 func main() {
 	buf, err := ioutil.ReadFile("11.txt")
@@ -189,34 +153,19 @@ func main() {
 		M1[i] = make([]byte, len(M[i]))
 	}
 
-	for n := 0; n < 100000; n++ {
-		pf("%d %d\n", n, allocc())
-		if part2 {
-			stepP2()
-		} else {
-			step()
-		}
-		if debug {
-			for i := range M {
-				pf("%s\n", string(M[i]))
-			}
-			pf("\n")
-		}
-		if nochange() {
-			break
-		}
+	savedM := make([][]byte, len(M))
+	for i := range M {
+		savedM[i] = make([]byte, len(M[i]))
+		copy(savedM[i], M[i])
 	}
 
-	if debug {
-		for i := range M {
-			fmt.Printf("%s\n", string(M[i]))
-		}
-	}
+	loop(stepP1)
 
-	if part2 {
-		pf("PART 2: %d\n", allocc())
-	} else {
-		pf("PART 1: %d\n", allocc())
-	}
+	pf("PART 1: %d\n", allocc())
 
+	M = savedM
+
+	loop(stepP2)
+
+	pf("PART 2: %d\n", allocc())
 }
