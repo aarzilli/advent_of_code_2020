aff2eaa8b694447b0ed74913d6ad145d1e767223
aarzilli <alessandro.arzilli@gmail.com>
2020-12-13 06:33:12 +0100

    cleanup

--- 13.go
+++ 13.go
@@ -3,21 +3,15 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"strconv"
 	"strings"
-	"os"
-	"math/big"
 )
 
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-// returns x without the last character
-func nolast(x string) string {
-	return x[:len(x)-1]
 }
 
 // splits a string, trims spaces on every element
@@ -49,39 +43,29 @@ func vatoi(in []string) []int {
 	return r
 }
 
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func exit(n int) {
-	os.Exit(n)
-}
-
 func pf(fmtstr string, args ...interface{}) {
 	fmt.Printf(fmtstr, args...)
 }
 
 var one = big.NewInt(1)
- 
+
 func crt(a, n []*big.Int) (*big.Int, error) {
+	// Chinese Remainder Theorem solver
 	// stolen from rosetta code :)
-    p := new(big.Int).Set(n[0])
-    for _, n1 := range n[1:] {
-        p.Mul(p, n1)
-    }
-    var x, q, s, z big.Int
-    for i, n1 := range n {
-        q.Div(p, n1)
-        z.GCD(nil, &s, n1, &q)
-        if z.Cmp(one) != 0 {
-            return nil, fmt.Errorf("%d not coprime", n1)
-        }
-        x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
-    }
-    return x.Mod(&x, p), nil
+	p := new(big.Int).Set(n[0])
+	for _, n1 := range n[1:] {
+		p.Mul(p, n1)
+	}
+	var x, q, s, z big.Int
+	for i, n1 := range n {
+		q.Div(p, n1)
+		z.GCD(nil, &s, n1, &q)
+		if z.Cmp(one) != 0 {
+			return nil, fmt.Errorf("%d not coprime", n1)
+		}
+		x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
+	}
+	return x.Mod(&x, p), nil
 }
 
 func main() {
@@ -90,8 +74,7 @@ func main() {
 	lines := strings.Split(string(buf), "\n")
 	start := atoi(lines[0])
 	buses := vatoi(splitandclean(lines[1], ",", -1))
-	pf("%d %#v\n", start, buses)
-	
+
 	min := 0
 	for i := range buses {
 		if buses[i] > min {
@@ -100,12 +83,12 @@ func main() {
 	}
 	min++
 	minid := -1
-	
+
 	for _, id := range buses {
 		if id < 0 {
 			continue
 		}
-		
+
 		pos := start % id
 		if pos == 0 {
 			minid = id
@@ -118,12 +101,12 @@ func main() {
 			}
 		}
 	}
-	
+
 	pf("PART 1: %d %d %d\n", min, minid, min*minid)
-	
+
 	a := make([]*big.Int, 0)
 	n := make([]*big.Int, 0)
-	
+
 	for i, id := range buses {
 		if id < 0 {
 			continue
@@ -131,9 +114,7 @@ func main() {
 		a = append(a, big.NewInt(int64(id-i)))
 		n = append(n, big.NewInt(int64(id)))
 	}
-	
-	pf("%v %v\n", a, n)
-	
+
 	sol, err := crt(a, n)
 	pf("PART 2: %v %v\n", sol, err)
 }
