dff89c7f1f8b5a3d8eafd76f3edf304f9567ca5c
aarzilli <alessandro.arzilli@gmail.com>
2020-12-14 06:52:04 +0100

    cleanup

--- 14.go
+++ 14.go
@@ -5,18 +5,12 @@ import (
 	"io/ioutil"
 	"strconv"
 	"strings"
-	"os"
 )
 
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-// returns x without the last character
-func nolast(x string) string {
-	return x[:len(x)-1]
 }
 
 // splits a string, trims spaces on every element
@@ -44,17 +38,6 @@ func vatoi(in []string) []int {
 		must(err)
 	}
 	return r
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func exit(n int) {
-	os.Exit(n)
 }
 
 func pf(fmtstr string, args ...interface{}) {
@@ -113,29 +96,58 @@ func getnums(in string, hasneg, hasdot bool) []string {
 	return r
 }
 
-func enum(addr uint64, floating []int, mem map[uint64]uint64, val uint64) {
-	if len(floating) == 0 {
-		pf("\t%36b (decimal %d)\n", addr, addr)
-		mem[addr] = val
-		return
-	}
-	
-	x := floating[0]
-	bv := uint64(1<<x)
-	
-	enum(addr&^bv, floating[1:], mem, val)
-	enum(addr|bv, floating[1:], mem, val)
+var (
+	maskSet   uint64
+	maskClear uint64
+	floating  []int
+	mem1      = map[uint64]uint64{}
+	mem2      = map[uint64]uint64{}
+)
+
+const debug = false
+
+func assign1(addr, val uint64) {
+	// assignment operation, part 1 version
+	val |= maskSet
+	val = val &^ maskClear
+	mem1[addr] = val
 }
 
-const part2 = true
+func enum(addr uint64, floating []int, val uint64) {
+	if len(floating) == 0 {
+		if debug {
+			pf("\t%36b (decimal %d)\n", addr, addr)
+		}
+		mem2[addr] = val
+		return
+	}
+
+	bv := uint64(1 << floating[0])
+	enum(addr&^bv, floating[1:], val)
+	enum(addr|bv, floating[1:], val)
+}
+
+func assign2(addr, val uint64) {
+	// assignment operation, part 2 version
+	if debug {
+		pf("%36b\n", addr)
+	}
+	addr |= maskSet
+	if debug {
+		pf("%36b (decimal %d) = (floating %v)\n", addr, addr, floating)
+	}
+	enum(addr, floating, val)
+}
+
+func total(mem map[uint64]uint64) uint64 {
+	tot := uint64(0)
+	for _, v := range mem {
+		tot += v
+	}
+	return tot
+}
 
 func main() {
-	var maskSet uint64
-	var maskClear uint64
-	var floating []int
-	
-	mem := map[uint64]uint64{}
-	
 	buf, err := ioutil.ReadFile("14.txt")
 	must(err)
 	lines := strings.Split(string(buf), "\n")
@@ -146,16 +158,19 @@ func main() {
 		}
 		v := splitandclean(line, "=", -1)
 		if v[0] == "mask" {
-			pf("mask\t\t%s\n", v[1])
-			
+			if debug {
+				pf("mask\t\t%s\n", v[1])
+			}
+
 			maskClear = 0
 			maskSet = 0
-			
+			floating = floating[:0]
+
 			for i := range v[1] {
-				bv := uint64(1 << (len(v[1]) - i-1))
+				bv := uint64(1 << (len(v[1]) - i - 1))
 				switch v[1][i] {
 				case 'X':
-					// nothing
+					floating = append(floating, (len(v[1]) - i - 1))
 				case '1':
 					maskSet |= bv
 				case '0':
@@ -164,47 +179,24 @@ func main() {
 					panic("blah")
 				}
 			}
-			
-			maskFloat := ^(maskSet | maskClear) & ((1<<36)-1)
-			pf("maskSet\t\t%36b\n", maskSet)
-			pf("maskClear\t%36b\n", maskClear)
-			
-			floating = floating[:0]
-			for i := 0; i < 40; i++ {
-				if maskFloat & (1 << i) != 0 {
-					floating = append(floating, i)
-				}
+
+			if debug {
+				pf("maskSet\t\t%36b\n", maskSet)
+				pf("maskClear\t%36b\n", maskClear)
 			}
-			
 		} else {
 			w := getints(v[0], false)
 			addr := uint64(w[0])
-			//val, _ := strconv.ParseUint(v[1], 2, 64)
 			val := uint64(atoi(v[1]))
-			//pf("mem[%d] = %b (%d)\n", addr, val, val)
-			
-			if !part2 {
-				val |= maskSet
-				val = val &^ maskClear
-				
-				//pf("\tafter mask %b\n", val)
-				
-				mem[addr] = val
-			} else {
-				pf("%36b\n", addr)
-				addr |= maskSet
-				//pf("%36b\n", addr)
-				//addr = addr &^ maskClear
-				pf("%36b (decimal %d) = (floating %v)\n", addr, addr, floating)
-				enum(addr, floating, mem, val)
-				
+			if debug {
+				pf("mem[%d] = %b (%d)\n", addr, val, val)
 			}
+
+			assign1(addr, val)
+			assign2(addr, val)
 		}
 	}
-	
-		tot := uint64(0)
-		for _, v := range mem {
-			tot += v
-		}
-		pf("PART 1: %d\n", tot)
+
+	pf("PART 1: %d\n", total(mem1))
+	pf("PART 2: %d\n", total(mem2))
 }
