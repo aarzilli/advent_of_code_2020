ca0c6934a250966df4aa1c4835e702fc54f19dad
aarzilli <alessandro.arzilli@gmail.com>
2020-12-24 11:03:41 +0100

    cleanup

--- 24.go
+++ 24.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strconv"
 	"strings"
-	"os"
 )
 
 func must(err error) {
@@ -14,54 +12,11 @@ func must(err error) {
 	}
 }
 
-// returns x without the last character
-func nolast(x string) string {
-	return x[:len(x)-1]
-}
-
-// splits a string, trims spaces on every element
-func splitandclean(in, sep string, n int) []string {
-	v := strings.SplitN(in, sep, n)
-	for i := range v {
-		v[i] = strings.TrimSpace(v[i])
-	}
-	return v
-}
-
-// convert string to integer
-func atoi(in string) int {
-	n, err := strconv.Atoi(in)
-	must(err)
-	return n
-}
-
-// convert vector of strings to integer
-func vatoi(in []string) []int {
-	r := make([]int, len(in))
-	for i := range in {
-		var err error
-		r[i], err = strconv.Atoi(in[i])
-		must(err)
-	}
-	return r
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func exit(n int) {
-	os.Exit(n)
-}
-
 func pf(fmtstr string, args ...interface{}) {
 	fmt.Printf(fmtstr, args...)
 }
 
-var prefixes = []string{ "e", "se", "sw", "w", "nw", "ne" }
+var prefixes = []string{"e", "se", "sw", "w", "nw", "ne"}
 
 type Point struct {
 	i, j int
@@ -101,17 +56,15 @@ func countblacknbrs(p Point) int {
 	return tot
 }
 
-
-
 func step() {
 	NewM := make(map[Point]bool)
-	
+
 	mini := 0
 	minj := 0
 	maxi := 0
 	maxj := 0
 	first := true
-	
+
 	for p := range M {
 		if first {
 			mini = p.i
@@ -133,10 +86,10 @@ func step() {
 			maxj = p.j
 		}
 	}
-	
-	for i := mini-1; i <= maxi+1; i++ {
-		for j := minj-1; j <= maxj+1; j++ {
-			p := Point{i,j}
+
+	for i := mini - 1; i <= maxi+1; i++ {
+		for j := minj - 1; j <= maxj+1; j++ {
+			p := Point{i, j}
 			if M[p] { // black
 				bnbs := countblacknbrs(p)
 				if bnbs == 0 || bnbs > 2 {
@@ -154,7 +107,7 @@ func step() {
 			}
 		}
 	}
-	
+
 	M = NewM
 }
 
@@ -177,8 +130,7 @@ func main() {
 		if line == "" {
 			continue
 		}
-		//pf("line: %s\n", line)
-		cur := Point{0,0}
+		cur := Point{0, 0}
 		for line != "" {
 			var instr string
 			for _, pfx := range prefixes {
@@ -190,16 +142,13 @@ func main() {
 			}
 			cur = tonbr(instr, cur)
 		}
-		//pf("pos %v: %v -> %v\n", cur, M[cur], !M[cur])
 		M[cur] = !M[cur]
 	}
-	
+
 	pf("PART 1: %d\n", countblack())
-	
+
 	for cnt := 0; cnt < 100; cnt++ {
 		step()
 	}
 	pf("PART 2: %d\n", countblack())
-	
-	
 }
